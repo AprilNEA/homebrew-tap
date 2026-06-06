8a513894e9feb8a82a7c18a05917f6d959fd5079
AprilNEA <github@sku.moe>
2026-06-06 20:21:07 +0800

    Update openlogi@latest to 0.5.3

--- Casks/openlogi@latest.rb
+++ Casks/openlogi@latest.rb
@@ -1,9 +1,9 @@
 cask "openlogi@latest" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.5.1"
-  sha256 arm:   "e2efe940a5794683fe20bf2edc021a8fb009235acc918c122d69d4f83d7a67d4",
-         intel: "ce63e26e88bf6a4746790b77f09be3ffd482d720f6df83ef23654ccbf93ca353"
+  version "0.5.3"
+  sha256 arm:   "de83a7a80e5dea05fcccee25897d0e554b35cdfd6ab8b9c9ddaafb561a8d439c",
+         intel: "bf236f0ead2d9e68aaac9f46e804953cfe70f452abbe4d2aca346d3ba027e647"
 
   url "https://github.com/AprilNEA/OpenLogi/releases/download/v#{version}/OpenLogi-v#{version}-macos-#{arch}.dmg"
   name "OpenLogi"
